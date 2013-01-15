0ba285e8b4040b79de014bbdeba790f96f446331
cws_rug <cws_rug@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-15 12:55:47 +0000

    committing again
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/netsim@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/NetSim/NetSim/src/create_process_state.cpp
+++ pkg/NetSim/NetSim/src/create_process_state.cpp
@@ -13,6 +13,7 @@ SEXP create_process_state(SEXP n) {
 	Rcpp::CharacterVector name(n);
 	// TODO Why does this cause an error in package building?
 	// possible reason: link sub folders for builder
+	// testing commit
 	// ProcessState * processState = new ProcessState();
 	Rcpp::NumericVector v1(2);
 	v1[0] = 1;
