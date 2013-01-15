b70bfcb546baff4f3df92c6ad60b008d2874ecb4
cws_rug <cws_rug@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-15 13:07:33 +0000

    deleted all?
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/netsim@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/NetSim/NetSim/R/create_process_state.R
+++
@@ -1,4 +0,0 @@
-create_process_state <- function(name = "foo"){
-	.Call( "create_process_state", name, PACKAGE = "NetSim" )
-}
-
--- pkg/NetSim/NetSim/R/test_call_main.R
+++
@@ -1,9 +0,0 @@
-# TODO: Add comment
-# 
-# Author: cws
-###############################################################################
-
-
-test_call_main <- function(){
-	.Call("main", PACKAGE = "NetSim")
-} 
